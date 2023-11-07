5c6da2c606a47ee597ecf362c017878623897cba
Michael Fehrer <mtfehrer@gmail.com>
2023-11-07 18:47:21 -0500

    sdfasdf

--- src/DirectMapped.cpp
+++ src/DirectMapped.cpp
@@ -15,7 +15,7 @@ void DirectMapped::processInstruction1KB(char instructionType, unsigned long lon
     int addressBits = 32;
     int offsetBits = 5;
     int indexBits = 5;
-    // int tagBits = addressBits - offsetBits - indexBits;
+    int tagBits = addressBits - offsetBits - indexBits;
 
     // unsigned int offset = addr & ((int)pow(2, offsetBits) - 1);
     unsigned int index = (addr >> offsetBits) & ((int)pow(2, indexBits) - 1);
@@ -24,14 +24,14 @@ void DirectMapped::processInstruction1KB(char instructionType, unsigned long lon
     if (instructionType == 'S')
     {
         // set tag
-        cache1KB[index] = tag << (offsetBits + indexBits);
+        cache1KB[index] = tag;
         // set valid bit to 1
         cache1KB[index] |= (1 << (addressBits - 1));
     }
     else if (instructionType == 'L')
     {
         unsigned int validBit = cache1KB[index] >> (addressBits - 1);
-        unsigned int tagInCache = (cache1KB[index] << 1) >> (offsetBits + indexBits);
+        unsigned int tagInCache = cache1KB[index] & ((int)pow(2, tagBits) - 1);
 
         if (validBit == 1 && tagInCache == tag)
         {
